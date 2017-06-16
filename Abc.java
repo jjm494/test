c11fa2ae1cf8a1e885d37a04b61e24da997881b9
jjm494 <skull494@naver.com>
2017-06-16 19:29:03 +0900

    출력명령어 한줄추가

--- Abc.java
+++ Abc.java
@@ -1,10 +1,11 @@
-package asd;
+﻿package asd;
 
 public class Abc {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("자바");
+		System.out.println("테스트1");
 	}
 
 }
